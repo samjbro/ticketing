aa5a24175b80ef787ccb590a673e8cf01a6918ce
sambroughton <sjbroughton1@gmail.com>
2020-10-25 19:10:49 -0500

    Added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  console.log("Auth starting up...");
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
 
